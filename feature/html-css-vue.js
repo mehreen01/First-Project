370e831398d9f03efc12ba4cb17203014aac4354
Mehreen Zahid <133596686+mehreen01@users.noreply.github.com>
2023-06-07 18:52:57 +0500

    index.html

---
+++ feature/html-css-vue.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Devops-Frontend</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <div id="app"></div>    
+    </body>
+</html>
